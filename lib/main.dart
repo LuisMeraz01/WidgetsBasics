a480f37b31e0db09cb37aa4431c4ca260ad6f12a
LuisMeraz01 <luismerazbarajas@gmail.com>
2024-02-12 12:34:29 -0600

    Cambio en ruta inicial

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BasicWidgetss(),
+      initialRoute: 'page1',
       routes: {
         'page1': (context) => const BasicWidgetss(),
         'page2': ((context) => const SecondSreecn())
